5352b0f978c248b608280210036126fbd2b89e75
tlgaNotenboom <tlganote@avans.nl>
2018-12-04 10:56:16 +0100

    user controller logic for deleting users

--- test/test_user.js
+++ test/test_user.js
@@ -49,4 +49,42 @@ describe('Creating users', () =>{
             })
         })
     })
-});
+    it('Should throw a status 412 if the password is invalid', done => {
+        request(app)
+        .post('/api/user')
+        .send({
+            username: "createdTestUser",
+            password: "E"
+            })
+        .end((err, res) => {
+            User.find({
+                username: "createdTestUser",
+                password: "E"
+            })
+            .then((foundUsers)=> {
+                assert(foundUsers.length === 0)
+                assert(res.status === 412)
+                done()
+            })
+            .catch((err) => {
+                done(err)
+            })
+        })
+    })
+})
+describe('Deleting users', () =>{
+    it('should delete a user if the correct id has been given', done => {
+        User.findOne()
+        .then((user) =>{
+            request(app)
+            .put("/api/user")
+            .send({
+                username: "testUser",
+                password: "123"
+            })
+            .end((err, res) =>{
+                assert(res.status === 200)
+            })
+        })
+    })
+})
