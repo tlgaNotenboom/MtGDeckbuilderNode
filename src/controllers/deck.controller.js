bf86e94a11186135326429bd1d08f47fa16940f7
tlgaNotenboom <tlganote@avans.nl>
2018-12-04 12:45:23 +0100

    template + routes

--- src/controllers/deck.controller.js
+++ src/controllers/deck.controller.js
@@ -2,5 +2,21 @@ const ApiError = require('../ApiError')
 const Deck = require('../models/deck');
 
 module.exports = {
+
+    getAllDecks(){
+
+    },
+    getSpecificDecks(){
+        
+    },
+    addDecks(){
+        
+    },
+    editDecks(){
+        
+    },
+    removeDecks(){
+        
+    }
     
 }
