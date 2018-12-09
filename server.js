1ac6b5c2dc94614738888987ba202f9db60c9600
tlgaNotenboom <tlganote@avans.nl>
2018-12-09 11:28:15 +0100

    Allowed CORS access from heroku app

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json())
 app.use(morgan("dev"));
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200' || "https://client-side-deckbuilder-app.herokuapp.com/");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
