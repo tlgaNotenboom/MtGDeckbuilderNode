fbc7c8c2e168bcd9d9d55d37d796d55cf349c915
tlgaNotenboom <tlganote@avans.nl>
2018-12-11 09:19:19 +0100

    removed run validator in deck.controller

--- src/controllers/deck.controller.js
+++ src/controllers/deck.controller.js
@@ -91,7 +91,6 @@ module.exports = {
                     }    
                 },
                 {
-                    runValidators: true,
                     new: true
                 })
             }
