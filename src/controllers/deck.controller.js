6a942f6cb51926aae0a8d19d673d75cd9895fec5
tlgaNotenboom <tlganote@avans.nl>
2018-12-12 12:09:59 +0100

    double oops

--- src/controllers/deck.controller.js
+++ src/controllers/deck.controller.js
@@ -110,7 +110,7 @@ module.exports = {
             if(foundDeck.length === 0){
                 throw new ApiError("Deck not found", 422);
             }else{
-                return Deck.findByIdAndDelete(foundDeck._id)
+                return Deck.findByIdAndDelete(foundDeck[0]._id)
             }
         })
         .then(() => {
