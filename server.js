fc2e32413ac94f5d66382668164d5d6907a8e910
Thomas Notenboom <tlganote@student.avans.nl>
2019-04-16 13:57:53 +0200

    commented access-control-allow-origin

--- server.js
+++ server.js
@@ -29,13 +29,13 @@ app.use(bodyParser.json())
 
 app.use(morgan("dev"));
 
-app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*'); // 'http://localhost:4200');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization,Authorization');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-});
+// app.use(function (req, res, next) {
+// 	res.setHeader('Access-Control-Allow-Origin', '*'); // 'http://localhost:4200');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,authorization,Authorization');
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	next();
+// });
 
 
 app.use('/api', auth_routes)
